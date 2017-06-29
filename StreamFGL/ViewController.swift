202c692b956ff798d49672eb7bd841eb4cd552ea
romanfurman6 <roman.furman@uptech.team>
2017-06-29 12:54:58 +0300

    Initial Commit

---
+++ StreamFGL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StreamFGL
+//
+//  Created by Roman Furman on 6/29/17.
+//  Copyright © 2017 Roman Furman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
