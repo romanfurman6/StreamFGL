2ced2ea08cc5f19d3341368961940d41bfdc3947
romanfurman6 <roman.furman@uptech.team>
2017-06-30 00:45:34 +0300

    add appdelegate

---
+++ StreamFGL/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  StreamFGL
+//
+//  Created by Roman Furman on 6/30/17.
+//  Copyright © 2017 Roman Furman. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    private var appCoordinator: CoordinatorProtocol?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        startAppCoordinator()
+
+        return true
+    }
+
+    private func startAppCoordinator() {
+        appCoordinator = AppCoordinator()
+        appCoordinator?.start()
+    }
+
+}
